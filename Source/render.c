6dbbeef14766f6291e110f8dbcb22e14511870e9
jelly <jellyzone@gmail.com>
2024-10-18 10:08:19 +0800

    try to get instance done

--- Source/render.c
+++ Source/render.c
@@ -7,7 +7,7 @@
 #include "flecs.h"
 
 static struct {
-    sg_pass_action pass_action;
+    
     int width;
     int height;
 } state;
@@ -16,25 +16,221 @@ static struct {
 static ecs_world_t* world;
 
 
-typedef struct {
-    float vertices[9]; // 三个顶点，每个顶点有 x, y, z
-} Triangle;
+//typedef struct {
+//    float vertices[9]; // 三个顶点，每个顶点有 x, y, z
+//} Triangle;
 
 
 
 
 typedef struct {
-    sg_buffer vertex_buffer;
+    sg_pass_action pass_action;
     sg_pipeline pipeline;
-} RenderData;
+} SokolCanvas;
+
+
+typedef struct {
+    float m[16]; // 4x4 变换矩阵
+} Transform;
+
+typedef struct {
+    float r, g, b, a;
+} Color;
+
+typedef struct SokolBuffer {
+    /* GPU buffers */
+    sg_buffer vertex_buffer;        /* Geometry (static) */
+    sg_buffer index_buffer;         /* Indices (static) */
+    sg_buffer color_buffer;         /* Color (per instance) */
+    sg_buffer transform_buffer;     /* Transform (per instance) */
+
+    /* Application-cached buffers */
+    Color* colors;
+    Transform* transforms;
+
+    /* Number of instances */
+    int32_t instance_count;
+
+    /* Number of indices */
+    int32_t index_count;
+} SokolBuffer;
 
 
 
 
 // 声明组件
-ECS_COMPONENT_DECLARE(Triangle);
+//ECS_COMPONENT_DECLARE(Triangle);
 ECS_COMPONENT_DECLARE(RenderData);
+ECS_COMPONENT_DECLARE(SokolBuffer);
 
+
+
+static
+sg_pass_action init_pass_action(
+    const Color bg_color)
+{
+  
+
+    return (sg_pass_action) {
+        .colors[0] = {
+            .load_action = SG_LOADACTION_CLEAR,
+            .clear_value = {
+                bg_color.r,
+                bg_color.g,
+                bg_color.b,
+                bg_color.a,
+            }
+        }
+    };
+}
+
+
+
+
+static
+sg_pipeline init_pipeline(void) {
+    /* create a shader (use vertex attribute locations) */
+    sg_shader shd = sg_make_shader(&(sg_shader_desc) {
+        .vs.source =
+            "#version 330\n"
+            "layout(location=0) in vec4 position;\n"
+            "layout(location=1) in vec4 color0;\n"
+            "layout(location=2) in mat4 mat;\n"
+            "out vec4 color;\n"
+            "void main() {\n"
+            "  gl_Position = mat * position;\n"
+            "  color = color0;\n"
+            "}\n",
+            .fs.source =
+            "#version 330\n"
+            "in vec4 color;\n"
+            "out vec4 frag_color;\n"
+            "void main() {\n"
+            "  frag_color = color;\n"
+            "}\n"
+    });
+
+    /* create a pipeline object (default render state is fine) */
+    return sg_make_pipeline(&(sg_pipeline_desc) {
+        .shader = shd,
+            .index_type = SG_INDEXTYPE_UINT16,
+            .layout = {
+                .buffers = {
+                    [1] = {.stride = 16, .step_func = SG_VERTEXSTEP_PER_INSTANCE },
+                    [2] = {.stride = 64, .step_func = SG_VERTEXSTEP_PER_INSTANCE }
+                },
+
+                .attrs = {
+                    /* Static geometry */
+                    [0] = {.offset = 0,  .format = SG_VERTEXFORMAT_FLOAT3 },
+
+                    /* Color buffer (per instance) */
+                    [1] = {.buffer_index = 1,  .offset = 0, .format = SG_VERTEXFORMAT_FLOAT4 },
+
+                    /* Matrix (per instance) */
+                    [2] = {.buffer_index = 2,  .offset = 0,  .format = SG_VERTEXFORMAT_FLOAT4 },
+                    [3] = {.buffer_index = 2,  .offset = 16, .format = SG_VERTEXFORMAT_FLOAT4 },
+                    [4] = {.buffer_index = 2,  .offset = 32, .format = SG_VERTEXFORMAT_FLOAT4 },
+                    [5] = {.buffer_index = 2,  .offset = 48, .format = SG_VERTEXFORMAT_FLOAT4 }
+                }
+        }
+    });
+}
+
+
+
+
+
+static
+void init_buffers(
+    ecs_world_t* world)
+{
+    ecs_entity_t rect_buf = ecs_lookup_fullpath(
+        world, "flecs.systems.sokol.RectangleBuffer");
+    ecs_assert(rect_buf != 0, ECS_INTERNAL_ERROR, NULL);
+
+    ecs_entity_t sokol_buffer = ecs_lookup_fullpath(
+        world, "flecs.systems.sokol.Buffer");
+    ecs_assert(sokol_buffer != 0, ECS_INTERNAL_ERROR, NULL);
+
+    SokolBuffer* b = ecs_get_mut_w_entity(world, rect_buf, sokol_buffer, NULL);
+    ecs_assert(b != NULL, ECS_INTERNAL_ERROR, NULL);
+
+    vec3 vertices[] = {
+        {-0.5, -0.5, 0.0},
+        { 0.5, -0.5, 0.0},
+        { 0.5,  0.5, 0.0},
+        {-0.5,  0.5, 0.0}
+    };
+
+    uint16_t indices[] = {
+        0, 1, 2,
+        0, 2, 3
+    };
+
+    b->vertex_buffer = sg_make_buffer(&(sg_buffer_desc) {
+        .size = sizeof(vertices),
+            .content = vertices,
+            .usage = SG_USAGE_IMMUTABLE
+    });
+
+    b->index_buffer = sg_make_buffer(&(sg_buffer_desc) {
+        .size = sizeof(indices),
+            .content = indices,
+            .type = SG_BUFFERTYPE_INDEXBUFFER,
+            .usage = SG_USAGE_IMMUTABLE
+    });
+
+    b->index_count = 6;
+}
+
+static
+void SokolSetCanvas(ecs_iter_t* it) {
+
+
+    for (int i = 0; i < it->count; i++) {
+        // 使用 ecs_get 获取 Triangle 组件
+        SokolBuffer* b = ecs_get(it->world, it->entities[i], SokolBuffer);
+
+        if (!b) {
+ 
+            continue;
+        }
+
+        vec3 vertices[] = {
+       {-0.5, -0.5, 0.0},
+       { 0.5, -0.5, 0.0},
+       { 0.5,  0.5, 0.0},
+       {-0.5,  0.5, 0.0}
+        };
+
+        uint16_t indices[] = {
+            0, 1, 2,
+            0, 2, 3
+        };
+
+        b->vertex_buffer = sg_make_buffer(&(sg_buffer_desc) {
+            .size = sizeof(vertices),
+                .data = SG_RANGE(vertices),
+                .usage = SG_USAGE_IMMUTABLE
+        });
+
+        b->index_buffer = sg_make_buffer(&(sg_buffer_desc) {
+            .size = sizeof(indices),
+                .data = SG_RANGE(indices),
+                .type = SG_BUFFERTYPE_INDEXBUFFER,
+                .usage = SG_USAGE_IMMUTABLE
+        });
+
+        b->index_count = 6;
+
+
+
+    }
+
+
+
+}
 
 
 // InitRenderData 观察者
